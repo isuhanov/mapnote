8e20762539c8726273d59431766fe5771d5daef5
tuc-nado <signat1911@gmail.com>
2022-03-30 01:09:23 +0300

    complete homework4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { AttributeDirective } from './attribute.directive';
+import { CurrencyPipe } from './currency.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     AttributeDirective,
+    CurrencyPipe,
   ],
   imports: [
     BrowserModule
