44193a300895b0bc82ae5169664630cc352e32cd
tuc-nado <signat1911@gmail.com>
2022-04-03 19:34:08 +0300

    complete homework6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, InjectionToken, VERSION } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TimerService } from './timer.service';
 
 export const DOCUMENT = new InjectionToken("__DOCUMENT__")
@@ -14,6 +15,35 @@ export class AppComponent implements AfterViewInit{
 
   name = 'Angular ' + VERSION.major;
   constructor(private timerService: TimerService) {}
+
+
+  public productFormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    id: new FormControl(this.generateId()),
+    price: new FormControl(0, [Validators.required, Validators.min(1)]),
+    categories: new FormControl([])
+  })
+
+  public generateId(): string{
+    var letters = "abcdefghijklmnopqrstuvwxyz";
+    var randStr = "";
+    while (randStr.length < 6) {
+      randStr += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return randStr;
+  }
+
+  public setId(): void{
+    this.productFormGroup.get('id').setValue(this.generateId());
+  }
+
+  public onSubmit(): void{
+    if(this.productFormGroup.invalid){
+      alert('Форма не валидна');
+      return;
+    }
+    console.log(this.productFormGroup.value);
+  }
 
   public ngAfterViewInit() {
     this.timerService.start();
